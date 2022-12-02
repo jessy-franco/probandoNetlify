066a26b31a62e1c978454e17f4102c71580d706f
JessyFranco <jesicafranco_1518@outlook.es>
2022-12-01 22:57:23 -0300

    Mejorando css

--- src/components/CartView/CartView.jsx
+++ src/components/CartView/CartView.jsx
@@ -8,6 +8,8 @@ import { useNavigate } from "react-router-dom";
 import swal from 'sweetalert';
 import "../CartView/cartView.css"
 import BuyForm from "../Forms/BuyForms";
+import Table from 'react-bootstrap/Table';
+
 
 /* 1-conectarlo al context---importando el context y el useContext */
 /* 2-mostrar el listado de items del array guardado en el context */
@@ -53,27 +55,42 @@ function CartView() {
 
     return (
         <section >
-            <h1>Tu carrito:</h1>
-            {/* siempre q hacemos un map pasamos una key */}
+            <h2>Tu carrito:</h2>
             <section className="vistaCart">
                 {cart.map((cartItem) => (
-                    <div key={cartItem.id} >
-                        <img src={cartItem.img} alt="Imagenes ilustrativas de los productos del carrito" width={"30%"} height={"auto"} />
-                        <h3>{cartItem.name}</h3>
-                        <h4>${cartItem.price}</h4>
-                        <h4>Cantidad: {cartItem.count}</h4>
-                        <h4>Precio total del producto: ${cartItem.count * cartItem.price}</h4>
-                        <Button onClick={() => removeItem(cartItem.id)} color="whiteSmoke" bGColor="#ff0101c2">X</Button>
-                    </div>
-                ))
-                }
+                    <Table key={cartItem.id} striped="columns" style={{width: "80%", height: "auto", margin: "0 5%"}}>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th style={{width: "auto", height: "auto",}}>Imagen</th>
+                                <th style={{width: "50%", height: "auto", textOverflow: "ellipsis"}}>Nombre</th>
+                                <th style={{width: "40%", height: "auto",}}>Precio</th>
+                                <th style={{width: "auto", height: "auto",}}>Cantidad</th>
+                                <th style={{width: "auto", height: "auto",}}>Precio total del producto</th>
+                                <th style={{width: "30%", height: "auto",}}>Remover item</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>*</td>
+                                <td><img src={cartItem.img} alt="Imagenes ilustrativas de los productos del carrito" width={"120px"} height={"auto"} /></td>
+                                <td><h4 style={{fontSize:"1.8rem", padding:"0 2%"}}>{cartItem.name}</h4></td>
+                                <td><h4>${cartItem.price}</h4></td>
+                                <td><h4>{cartItem.count}</h4></td>
+                                <td><h4> ${cartItem.count * cartItem.price}</h4></td>
+                                <td><Button onClick={() => removeItem(cartItem.id)} color="whiteSmoke" bGColor="#ff0101c2">X</Button></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                    
+                ))}
                 <Button onClick={clear}>Vaciar Carrito</Button>
                 <div><h5>El total de su compra es: ${totalPriceInCart()}</h5></div>
-                <Link to ="/" >
-                <Button>Volver a productos...</Button>
+                <Link to="/" >
+                    <Button>Volver a productos...</Button>
                 </Link>
                 <BuyForm onSubmit={createBuyOrder} />
-                
+
             </section>
         </section>
     )
